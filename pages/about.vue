46991de7a0ae46191816c8b9c1008c6e9ce6ef86
Arnas Boruchovas <aboruchovas@gmail.com>
2020-08-02 21:41:21 +0100

    Add content for about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,8 +1,33 @@
 <template>
   <div>
     <h1 class="title text-center display-3 pt-4">
-      <a href="/" class="text-dark">Arnas Boruchovas</a>
+      <a href="/">Arnas Boruchovas</a>
     </h1>
+
+    <div class="about p-5">
+      <div>
+        <p class="pt-4">
+          Hi there! 👋
+        </p>
+        <p>
+          I’m a third year Computer Science student at the 🎓 University of
+          Sheffield, currently completing a summer internship at Tribepad.
+        </p>
+        <p>
+          I’m passionate about designing and creating elegant solutions using 👨‍💻
+          modern technologies.
+        </p>
+      </div>
+
+      <div class="pt-5">
+        <p>
+          I'm also the Trips Officer 🚀 for hackSheffield, a society at the
+          University of Sheffield ran by students. We organise a yearly
+          hackathon open to students around the world, as well as visiting other
+          hackathons!
+        </p>
+      </div>
+    </div>
 
     <NavBar />
   </div>
@@ -20,8 +45,16 @@ export default {
 
 <style>
 .title {
-  font-family: 'Roboto';
-  font-weight: 500;
+  font-family: 'Roboto', sans-serif;
+}
+
+.title a {
+  color: black;
+}
+
+.about {
+  font-family: 'Roboto', sans-serif;
+  font-size: 2.5em;
 }
 
 a {
